fd8508094d200fd0a0f311787a0d6de19e5d09a9
unknown <kenneth_oafallas@dlsu.edu.ph>
2021-03-29 23:42:27 +0800

    scaffolding

---
+++ CSOPESY-Project1-S15-Group16.c
@@ -0,0 +1,22 @@
+/***********************************************
+Names:  TAN, Darren
+        OAFALLAS, Kenneth Neil
+Group: 16
+Section: S15
+***********************************************/
+
+#include "stdio.h"
+#include "stdlib.h"
+
+#include "headers/process.h"
+#include "headers/queue.h"
+
+#include "headers/fcfs.h"
+#include "headers/nsjf.h"
+#include "headers/psjf.h"
+#include "headers/rr.h"
+
+int main (void) {
+   printf("Hello World!");
+   return 0;
+}
