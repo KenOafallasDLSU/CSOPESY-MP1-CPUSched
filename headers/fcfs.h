ba66e5ea0e0516e374e774740dafea3d2bfbe5f9
Darren Tan <50022686+Renkuro@users.noreply.github.com>
2021-04-01 22:37:20 +0800

    added printf()s for output

--- headers/fcfs.h
+++ headers/fcfs.h
@@ -5,3 +5,21 @@ Group: 16
 Section: S15
 ***********************************************/
 
+void
+fcfs(int nprocesses, int process_id_list[], int arrival_time_list[], int total_execution_time[])
+{
+        float awt;
+        
+        int i;
+        
+        for(i = 0; i < nprocesses; i++)
+        {
+                //printf("P[%d]\n", process_id_list[i]);
+                //printf("Start Time: %d End time: %d\n", , );
+                //printf("Waiting time: %d\n", );
+                //printf("Turnaround time: %d\n", );
+                //printf("************************************\n");
+        }
+        
+        //printf("Average waiting time: %.1f", awt);
+}
