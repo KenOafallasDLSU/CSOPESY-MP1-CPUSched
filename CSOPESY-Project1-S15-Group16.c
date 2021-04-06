af7324dd9e8c7f2f9bf08e83613cc53660308ef1
Darren Tan <50022686+Renkuro@users.noreply.github.com>
2021-04-06 22:08:54 +0800

    added fcfs

--- CSOPESY-Project1-S15-Group16.c
+++ CSOPESY-Project1-S15-Group16.c
@@ -9,25 +9,27 @@ Section: S15
 #include <stdlib.h>
 #include <string.h>
 
-#include "headers/process.h"
-#include "headers/queue.h"
-
+//#include "headers/process.h"
+//#include "headers/queue.h"
 #include "headers/fcfs.h"
-#include "headers/nsjf.h"
-#include "headers/psjf.h"
-#include "headers/rr.h"
+//#include "headers/nsjf.h"
+//#include "headers/psjf.h"
+//#include "headers/rr.h"
+
+#define MAX 100
 
 int 
 main(void) 
 {
     char filename[50], txt[] = ".txt";
     int algo, nprocesses, tsv;
-    int process_id, arrival_time, total_execution_time;
-    int process_id_list[], arrival_time_list[], total_execution_time[];
+    int process_id, arrival_time, exec_time;
+    int process_id_list[MAX], arrival_time_list[MAX], exec_time_list[MAX];
 
     printf("Enter file name: ");
     scanf("%s", filename);
-
+	printf("\n");
+	
     strcat(filename, txt);
 
     FILE* fp = fopen(filename,"r");
@@ -38,21 +40,21 @@ main(void)
         return 0;
     }
     
-    fscanf(fp, "%d %d %d", algo, nprocesses, tsv)
+    fscanf(fp, "%d %d %d", &algo, &nprocesses, &tsv);
     
-    int i;
-    
-    if(algo == 1)
+    if(algo == 0)
     {
-            for(i = 0; i < nprocesses; i++)
-            {
-                 fscanf(fp, "%d %d %d", process_id, arrival_time, total_execution_time);
-                 process_id_list[i] = process_id;
-                 arrival_time_list[i] = arrival_time;
-                 total_execution_time[i] = total_execution_time;
-            }
+    	int i;
+    	
+        for(i = 0; i < nprocesses; i++)
+        {
+            fscanf(fp, "%d %d %d", &process_id, &arrival_time, &exec_time);
+            process_id_list[i] = process_id;
+            arrival_time_list[i] = arrival_time;
+            exec_time_list[i] = exec_time;
+        }
             
-            fcfs(nprocesses, process_id_list, arrival_time_list, total_execution_time);
+        fcfs(nprocesses, process_id_list, arrival_time_list, exec_time_list);
     }
 
     fclose(fp);
