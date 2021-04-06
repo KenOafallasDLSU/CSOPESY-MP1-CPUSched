52ebaf4a3611069823778a9099cb82091b250a10
Darren Tan <50022686+Renkuro@users.noreply.github.com>
2021-04-06 22:07:17 +0800

    added fcfs function

--- headers/fcfs.h
+++ headers/fcfs.h
@@ -5,21 +5,31 @@ Group: 16
 Section: S15
 ***********************************************/
 
+#include <stdio.h>
+
 void
-fcfs(int nprocesses, int process_id_list[], int arrival_time_list[], int total_execution_time[])
+fcfs(int nprocesses, int process_id_list[], int arrival_time_list[], int exec_time_list[])
 {
-        float awt;
+	int total_exec_time = 0, total_wt = 0, start_time = 0;
+	float awt;
         
-        int i;
+    int i;
         
-        for(i = 0; i < nprocesses; i++)
-        {
-                //printf("P[%d]\n", process_id_list[i]);
-                //printf("Start Time: %d End time: %d\n", , );
-                //printf("Waiting time: %d\n", );
-                //printf("Turnaround time: %d\n", );
-                //printf("************************************\n");
-        }
+    for(i = 0; i < nprocesses; i++)
+    {
+    	total_exec_time += exec_time_list[i];
+    	
+        printf("P[%d]\n", process_id_list[i]);
+        printf("Start Time: %d End time: %d\n", start_time, total_exec_time);
+        printf("Waiting time: %d\n", start_time);
+        printf("Turnaround time: %d\n", total_exec_time);
+        printf("************************************\n");
         
-        //printf("Average waiting time: %.1f", awt);
+        total_wt += start_time;
+        start_time = total_exec_time;
+    }
+    
+	awt = total_wt / nprocesses;
+	    
+    printf("Average waiting time: %.1f", awt);
 }
