79ee01b54f9d66b72af1aec54a87059a3cd67004
Darren Tan <50022686+Renkuro@users.noreply.github.com>
2021-04-09 13:13:12 +0800

    scaffolding

---
+++ headers/mlfq.h
@@ -0,0 +1,14 @@
+/***********************************************
+Names:  TAN, Darren
+        OAFALLAS, Kenneth Neil
+Group: 16
+Section: S15
+***********************************************/
+
+#include <stdio.h>
+
+void
+mlfq()
+{
+	
+}
