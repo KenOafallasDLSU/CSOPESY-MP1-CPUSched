d2dedc24c2bb420b969001be5542ab3c35fa5516
unknown <kenneth_oafallas@dlsu.edu.ph>
2021-04-25 21:34:22 +0800

    Gantt for RR, PSJF

---
+++ headers/helpers.h
@@ -0,0 +1,129 @@
+/***********************************************
+Names:  TAN, Darren
+        OAFALLAS, Kenneth Neil
+Group: 16
+Section: S15
+***********************************************/
+int checkAllDone(int nProcesses, struct Process aProcesses[])
+{
+        int bAllDone = 1;
+
+	int i;
+        for(i=0; i<nProcesses; i++)
+                if(aProcesses[i].remainingTime != 0)
+                        bAllDone = 0;
+
+        return bAllDone;
+}
+
+int checkNewArrival(int loc, int systemTime, int nProcesses, struct Process aProcesses[])
+{
+        int found = -1;
+
+        int ctr = loc;
+        while(found <= -1 && ctr < nProcesses)
+        {
+                if(aProcesses[ctr].arrivalTime == systemTime)
+                        found = ctr;
+                ctr++;
+        }
+
+        return found;
+}
+
+void updateActiveProcess(struct Process *process)
+{
+        process->remainingTime--;
+}
+
+void updateWaitingProcess(struct Process *process)
+{
+        process->waitingTime++;
+        process->turnaroundTime++;
+}
+
+float getAvgWaitingTime(int nProcesses, struct Process aProcesses[])
+{
+	float sum = 0;
+	
+	int i;
+	for(i=0; i<nProcesses; i++)
+		sum += aProcesses[i].waitingTime;
+	
+	return sum/nProcesses;
+}
+
+void printProcessReport(struct Process *process)
+{
+	int i;
+		
+	printf("P[%d]\n", process->processID);
+	for(i=0; i<process->runCnt; i++)
+	        printf("Start time: %d End time: %d\n", *(process->aStart+i), *(process->aEnd+i));
+        printf("Waiting time: %d\n", process->waitingTime);
+        printf("Turnaround time: %d\n", process->turnaroundTime);
+        printf("************************************\n");
+}
+
+void printGantt(int nProcesses, struct Process aProcesses[])
+{
+        int systemTime = 0;
+        int doneProcess[nProcesses];
+        int allPrinted = 0;
+        int lastEnd = 0;
+
+        int i;
+        for(i=0; i<nProcesses; i++)
+                doneProcess[i] = 0;
+
+        printf("\nGANTT CHART\n");
+        do{
+                //check done printing
+                int allPrinted = 1;
+                for(i=0; i<nProcesses; i++)
+                {
+                        if(aProcesses[i].runCnt != doneProcess[i])
+                        {
+                                allPrinted = 0;
+                                break;
+                        }
+                        //else printf("!!!Done: %d\n", i);
+                        
+                        //printf("!!!Count: %d\n", doneProcess[i]);
+                }
+                if(allPrinted == 1) break;
+
+                //find next
+                for(i=0; i<nProcesses; i++)
+                {
+                        int k;
+                        for(k=0; k<aProcesses[i].runCnt; k++)
+                        {
+                                if(aProcesses[i].aStart[k] == systemTime)
+                                {
+                                		doneProcess[i]++;
+                                        if(aProcesses[i].aStart[k] != lastEnd)
+                                        {
+                                                printf("  +------ %d\n", lastEnd);
+                                                printf("  |       |\n");
+                                                printf("  | IDLE  |\n");
+                                                printf("  |       |\n");
+                                        }
+                                        printf("  +------ %d\n", aProcesses[i].aStart[k]);
+                                        printf("  |       |\n");
+                                        printf("  | P[%d]  |\n", aProcesses[i].processID);
+                                        printf("  |       |\n");
+
+                                        lastEnd = aProcesses[i].aEnd[k];
+                                }
+                        }
+                }
+
+				//update time
+				if(lastEnd > systemTime+1)
+					systemTime = lastEnd;
+				else
+					systemTime++;
+        } while(allPrinted == 0);
+        printf("  +-----+ %d\n", lastEnd);
+}
--- headers/psjf.h
+++ headers/psjf.h
@@ -4,33 +4,6 @@ Names:  TAN, Darren
 Group: 16
 Section: S15
 ***********************************************/
-int checkAllDone(int nProcesses, struct Process aProcesses[])
-{
-        int bAllDone = 1;
-
-		int i;
-        for(i=0; i<nProcesses; i++)
-                if(aProcesses[i].remainingTime != 0)
-                        bAllDone = 0;
-
-        return bAllDone;
-}
-
-int checkNewArrival(int loc, int systemTime, int nProcesses, struct Process aProcesses[])
-{
-        int found = -1;
-
-        int ctr = loc;
-        while(found <= -1 && ctr < nProcesses)
-        {
-                if(aProcesses[ctr].arrivalTime == systemTime)
-                        found = ctr;
-                ctr++;
-        }
-
-        return found;
-}
-
 int getShortestRemaining(int nArrivedCnt, int aArrived[], struct Process aProcesses[])
 {
 		if(nArrivedCnt == 0) return -1;
@@ -49,40 +22,6 @@ int getShortestRemaining(int nArrivedCnt, int aArrived[], struct Process aProces
 
 		if(aProcesses[shortest].remainingTime == 0) return -1;
         return shortest;
-}
-
-void updateActiveProcess(struct Process *process)
-{
-        process->remainingTime--;
-}
-
-void printProcessReport(struct Process *process)
-{
-		int i;
-		
-		printf("P[%d]\n", process->processID);
-		for(i=0; i<process->runCnt; i++)
-			printf("Start time: %d End time: %d\n", *(process->aStart+i), *(process->aEnd+i));
-        printf("Waiting time: %d\n", process->waitingTime);
-        printf("Turnaround time: %d\n", process->turnaroundTime);
-        printf("************************************\n");
-}
-
-void updateWaitingProcess(struct Process *process)
-{
-        process->waitingTime++;
-        process->turnaroundTime++;
-}
-
-float getAvgWaitingTime(int nProcesses, struct Process aProcesses[])
-{
-	float sum = 0;
-	
-	int i;
-	for(i=0; i<nProcesses; i++)
-		sum += aProcesses[i].waitingTime;
-	
-	return sum/nProcesses;
 }
 
 void psjf(int nProcesses, struct Process aProcesses[])
@@ -160,5 +99,5 @@ void psjf(int nProcesses, struct Process aProcesses[])
         }
         
         awt = getAvgWaitingTime(nProcesses, aProcesses);
-        printf("Average Waiting Time: %.1f", awt);
+        printf("Average Waiting Time: %.1f\n", awt);
 }
